f85043d5795bc8e2b649f1e6cad27a1019be4bf0
David L <david_mollosund@hotmail.com>
2016-06-12 18:08:24 +0200

    First commit test project.

---
+++ KaffeServer/Connection.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KaffeServer
+{
+    class Connection
+    {
+
+        TcpClient tcpClient;
+
+        private Thread threadSender;
+        private StreamReader srRecevier;
+        private StreamWriter swSender;
+        private string currentUser;
+        private string stringResponse;
+
+
+        public Connection(TcpClient tcpClientConnection)
+        {
+            tcpClient = tcpClientConnection;
+            threadSender = new Thread(AcceptClient);
+
+            threadSender.Start();
+        }
+
+        public void CloseConnection()
+        {
+            tcpClient.Close();
+            srRecevier.Close();
+            swSender.Close();
+           
+        }
+
+        public void AcceptClient()
+        {
+            srRecevier = new StreamReader(tcpClient.GetStream());
+            swSender = new StreamWriter(tcpClient.GetStream());
+
+            currentUser = srRecevier.ReadLine();
+
+            if (currentUser != "") 
+            {
+                if (Server.connectedUsers.Contains(currentUser) == true)
+                {
+                    swSender.WriteLine("0|This username already exists");
+                    swSender.Flush();
+                    CloseConnection();
+                    return;
+                }
+                else if (currentUser == "Administrator")
+                {
+                    swSender.WriteLine("0|This username is reserved.");
+                    swSender.Flush();
+                    CloseConnection();
+                    return;
+                }
+                else
+                {
+                    swSender.WriteLine("1");
+                    swSender.Flush();
+                    Server.AddUser(tcpClient, currentUser);
+                }
+                
+            }
+            else
+            {
+                CloseConnection();
+                return;
+            }
+
+            try
+            {
+                while ((stringResponse = srRecevier.ReadLine()) != "")
+                {
+                    if (stringResponse == null)
+                    {
+                        Server.RemoveUser(tcpClient);
+                    }
+                    else
+                    {
+                        Server.SendMessage(currentUser, stringResponse);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, " Removed user: " + tcpClient.ToString());
+
+                Server.RemoveUser(tcpClient);
+
+            }
+
+            
+        }
+
+    }
+}
---
+++ KaffeServer/Server.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Collections;
+
+namespace KaffeServer
+{
+    class Server
+    {
+        public static Hashtable connectedUsers = new Hashtable(30);
+        public static Hashtable connections = new Hashtable(30);
+        private IPAddress ipAddress;
+        private TcpClient tcpClient;
+
+        public Server(IPAddress ip)
+        {
+            ipAddress = ip;
+        }
+
+        private Thread threadListener;
+        private TcpListener tcpListenerClient;
+        bool ServerRunning = false;
+
+        public static void AddUser(TcpClient tcpUser, string userName)
+        {
+            connectedUsers.Add(connectedUsers, userName);
+            connections.Add(connections, tcpUser);
+
+            SendBroadcast(connections[userName] + " has connected");
+        }
+
+        private static void SendBroadcast(string message)
+        {
+            StreamWriter swSenderSender;
+            TcpClient[] tcpClients = new TcpClient[Server.connectedUsers.Count];
+
+            Server.connectedUsers.Values.CopyTo(tcpClients, 0);
+
+            for (int i = 0; i < tcpClients.Length; i++)
+            {
+                try
+                {
+                    if (message.Trim() == "" || tcpClients[i] == null)
+                    {
+                        continue;
+                    }
+                    swSenderSender = new StreamWriter(tcpClients[i].GetStream());
+                    swSenderSender.WriteLine("Admin: " + message);
+                    swSenderSender.Flush();
+                    swSenderSender = null;
+
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message, " Removed user: " + connectedUsers[i].ToString());
+
+                    RemoveUser(tcpClients[i]);
+                    
+                }
+            }
+        }
+
+        public static void SendMessage(string from, string message)
+        {
+            StreamWriter swSenderSender;
+
+            TcpClient[] tcpClients = new TcpClient[Server.connectedUsers.Count];
+            Server.connectedUsers.Values.CopyTo(tcpClients, 0);
+
+            for (int i = 0; i < tcpClients.Length; i++)
+            {
+                try
+                {
+                    if (message.Trim() == "" || tcpClients[i] == null)
+                    {
+                        continue;
+                    }
+                    swSenderSender = new StreamWriter(tcpClients[i].GetStream());
+                    swSenderSender.WriteLine(from + " whispers: " + message);
+                    swSenderSender.Flush();
+                    swSenderSender = null;
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message, " Removed user: " + connectedUsers[i].ToString());
+
+                    RemoveUser(tcpClients[i]);
+                }
+            }
+
+        }
+
+        public void StartListening()
+        {
+            IPAddress ipLocal = ipAddress;
+            tcpListenerClient = new TcpListener(19777);
+            tcpListenerClient.Start();
+
+            ServerRunning = true;
+
+            threadListener = new Thread(KeepListening); ;
+            threadListener.Start();
+        }
+
+        private void KeepListening()
+        {
+            while (ServerRunning == true)
+            {
+                tcpClient = tcpListenerClient.AcceptTcpClient();
+
+                Connection newConnection = new Connection(tcpClient);
+            }
+        }
+
+        public static void RemoveUser(TcpClient tcpUser)
+        {
+            if (connections[tcpUser] != null)
+            {
+                SendBroadcast(connections[tcpUser] + " has left");
+
+                Server.connectedUsers.Remove(Server.connections[tcpUser]);
+                Server.connections.Remove(tcpUser);
+            }
+        }
+        
+
+    }
+}
